5d67e241e7d2733a883ef73d91d614a3b9361dae
Albin Hubsch <albin.hubsch@gmail.com>
2017-08-20 17:19:25 +0200

    Added eagle and event migrations

---
+++ database/migrations/2017_08_20_150408_create_eagles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEaglesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eagles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('EagleEye');
+            $table->string('description');
+            $table->enum('state', ['armed', 'disarmed'])->default('disarmed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eagles');
+    }
+}
